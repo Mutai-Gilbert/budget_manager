994b8d643fba864bb922e0ff1013052611c72b01
mutai-gilbert <mutaigilbert85@gmail.com>
2023-07-13 06:34:20 +0300

    Create db and models setup

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,7 @@
 class Transaction < ApplicationRecord
-  belongs_to :category
+  belongs_to :User
+  has_and_belongs_to_many :categories
+
+  validates :name, presence: true
+  validates :amount, numericality: { greater_than: 0 }
 end
