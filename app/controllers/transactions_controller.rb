ee139f3c684f410c2540ba1e7bf32018521969f6
mutai-gilbert <mutaigilbert85@gmail.com>
2023-07-13 20:09:38 +0300

    Added a file app/controllers/transactions_controller.rb

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,3 +1,24 @@
 class TransactionsController < ApplicationController
-  def new; end
+  
+  def new
+    @category = current_user.categories.find(params[:category_id])
+    @transaction = Transaction.new
+  end
+
+  def create
+    @category = current_user.categories.find(params[:category_id])
+    @transaction = @category.transactions.new(transaction_params)
+    @transaction.user = current_user
+    if @transaction.save
+      redirect_to category_path(@category)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def transaction_params
+    params.require(:transaction).permit(:name, :amount)
+  end
 end
