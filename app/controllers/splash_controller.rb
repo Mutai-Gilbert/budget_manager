e1236cd34baa30f3cbcf7e8db6e7982890466d47
mutai-gilbert <mutaigilbert85@gmail.com>
2023-07-14 12:23:31 +0300

    Added changes in the app/controllers/splash_controller.rb file

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -1,7 +1,8 @@
 class SplashController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  
   def index
     return unless user_signed_in?
 
     redirect_to categories_path
+  end
 end
