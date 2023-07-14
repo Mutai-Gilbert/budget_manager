04a13b5f6fdd294254d3304d69813e2120f935dd
mutai-gilbert <mutaigilbert85@gmail.com>
2023-07-14 12:34:31 +0300

    Added changes to the config/routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "splash#index"
+
   resources :splash, only: [:index]
-  resources :users, only: %i[new create]
-  resources :sessions, only: %i[new create destroy]
-  resources :home, only: [:index] do
-    resources :categories, only: %i[new create]
-  end
-  resources :transactions, only: %i[index new create]
-  resources :categories, only: [] do
-    resources :transactions, only: %i[index new create]
+
+  resources :categories, only: [:index, :show, :new, :create] do
+    resources :transactions, only: [:new, :create]
   end
 end
