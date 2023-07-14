46a423592d2f5f4d8202b5dee6db79a9c936b7d5
mutai-gilbert <mutaigilbert85@gmail.com>
2023-07-14 12:22:57 +0300

    Added changes in app/controllers/categories_controller.rb

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,29 @@
 class CategoriesController < ApplicationController
-  def new; end
+  def index
+    @categories = current_user.categories
+  end
+
+  def show
+    @category = current_user.categories.find(params[:id])
+    @transactions = @category.transactions.order(created_at: :desc)
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = current_user.categories.new(category_params)
+    if @category.save
+      redirect_to categories_path, notice: 'Category was successfully created.'
+    else
+      render :new, alert: 'Category was not created.'
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
+  end
 end
