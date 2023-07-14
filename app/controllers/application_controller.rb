e483982d223714653b0f5ef6d0fa89e66f23e9ee
mutai-gilbert <mutaigilbert85@gmail.com>
2023-07-14 12:24:53 +0300

    Added app/controllers/application_controller.rb file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,18 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user_except_splash
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user_except_splash
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    private
+  private
 
-    def authenticate_user_except_splash
-        unless controller_name == 'splash' && action_name == 'index'
-            authenticate_user!
-        end
-    end
+  def authenticate_user_except_splash
+    return if controller_name == 'splash' && action_name == 'index'
 
+    authenticate_user!
+  end
 
-    protected
-    
-        def configure_permitted_parameters
-            devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-        end
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
